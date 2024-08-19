ecb05d14568b5448fcb674a90dff7feb370b86ba
Louise Eriksson <louiseriksson.92@gmail.com>
2024-08-19 08:57:42 +0200

    small design change

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     <div className={styles.homeWrap}>
         <h1>Welcome to Chatify</h1>
         <div className="cardWrapper">
-            <div className="card bg-base-100 w-96 shadow-xl shadow-cardShadow">
+            <div className="card bg-base-100 w-96 shadow-cardShadow">
                 <div className='card-body items-center text-center gap-7'>
                     <h2 className="card-title">What do you want to do?</h2>
                     <div className="card-actions justify-end">
